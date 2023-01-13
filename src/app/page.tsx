ad54cacf7c5bd4388447b0df21c131dafb5862b7
Alex Sanchez <alexs@glue.digital>
2023-01-13 16:41:41 +0100

    Added tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
+        <p className='text-4xl text-amber-400'>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
