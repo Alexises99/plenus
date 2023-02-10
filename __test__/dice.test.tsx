b5ee15f54d1b03101ef10ec59b21f684b6160de7
Alex Sanchez <alexs@glue.digital>
2023-02-10 17:12:42 +0100

    Added tests

---
+++ __test__/dice.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Dice from '@/components/Dices/Dice'
+
+// const dice: DiceData = {
+//   diceColor: 'red',
+//   diceNumber: 2
+// }
+
+const testNumber = 1
+
+describe('Dice', () => {
+  it('renders something', () => {
+    render(<Dice active children={testNumber} />)
+
+    const number = screen.getByText(testNumber)
+
+    expect(number).toBeInTheDocument()
+  })
+
+  it('renders dice unchanged', () => {
+    const { container } = render(<Dice active children={testNumber} />)
+    expect(container).toMatchSnapshot()
+  })
+})
--- src/components/Dices/Dice.tsx
+++ src/components/Dices/Dice.tsx
@@ -6,19 +6,21 @@ interface DiceProps {
 }
 
 export default function Dice({ active, children }: DiceProps) {
-  return (
-    <div
-      className={`border-2 rounded-full w-20 h-20 flex justify-center items-center cursor-pointer ${
-        active && 'border-green-400'
-      }`}
-    >
-      <span
-        className={`text-3xl font-bold text-white flex justify-center items-center ${
-          active && 'text-green-400'
-        }`}
-      >
-        {children}
-      </span>
-    </div>
-  )
+  if (active) {
+    return (
+      <div className="border-2 rounded-full w-20 h-20 flex justify-center items-center cursor-pointer bg-green-400">
+        <span className="text-3xl font-bold text-white flex justify-center items-center bg-green-400">
+          {children}
+        </span>
+      </div>
+    )
+  } else {
+    return (
+      <div className="border-2 rounded-full w-20 h-20 flex justify-center items-center cursor-pointer">
+        <span className="text-3xl font-bold text-white flex justify-center items-center">
+          {children}
+        </span>
+      </div>
+    )
+  }
 }
